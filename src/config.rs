00f58965fc22a7d8564ea2c980311c2829220042
Mark Shternberg <nikita@medvedev-it.ru>
2025-11-25 17:06:08 +0300

    Update rendezvous server and public key constants

--- src/config.rs
+++ src/config.rs
@@ -106,8 +106,8 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["rs-ny.rustdesk.com"];
-pub const RS_PUB_KEY: &str = "OeVuKk5nlHiXp+APNn0Y3pC1Iwpwn44JGqrQCsWqmBw=";
+pub const RENDEZVOUS_SERVERS: &[&str] = &["remote.medvedev-it.com"];
+pub const RS_PUB_KEY: &str = "tNNd2udBSqSnbKAd9s1kZli1cNlbl3YbTDOUTRLOfDE=";
 
 pub const RENDEZVOUS_PORT: i32 = 21116;
 pub const RELAY_PORT: i32 = 21117;
